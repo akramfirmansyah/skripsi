9d98bb9843b367363de88b5d57bf602ebe53d39c
akramfirmansyah <akram.firman@gmail.com>
2024-09-05 10:04:21 +0800

    remove mosquitto from docker compose

--- Pre/setup.sh
+++ Pre/setup.sh
@@ -3,9 +3,6 @@
 echo "Starting setup!"
 
 NODE_RED_DIR="node-red"
-MOSQUITTO_DIR="mosquitto"
-MOSQUITTO_CONFIG_DIR="$MOSQUITTO_DIR/config"
-MOSQUITTO_DATA_DIR="$MOSQUITTO_DIR/data"
 INFLUXDB_DIR="influxdb"
 
 # Check if directory node-red exists
@@ -17,42 +14,11 @@ else
   echo "Directory '$NODE_RED_DIR' created."
 fi
 
-# Check if directory mosquitto exists
-if [ -d "$MOSQUITTO_DIR" ]; then
-  echo "Directory '$MOSQUITTO_DIR' already exists."
-else
-  # Create directory mosquitto
-  mkdir -p "$MOSQUITTO_DIR"
-  echo "Directory '$MOSQUITTO_DIR' created." 
-fi
-
-# Check if directory config of mosquitto exists
-if [ -d "$MOSQUITTO_CONFIG_DIR" ]; then
-  echo "Directory '$MOSQUITTO_CONFIG_DIR' already exists."
-else
-  # Create directory config of mosquitto
-  mkdir -p "$MOSQUITTO_CONFIG_DIR"
-    echo "Directory '$MOSQUITTO_CONFIG_DIR' created."
-fi
-
-# Check if directory data of mosquitto exists
-if [ -d "$MOSQUITTO_DATA_DIR" ]; then
-  echo "Directory '$MOSQUITTO_DATA_DIR' already exists."
-else
-  # Create directory data of mosquitto
-  mkdir -p "$MOSQUITTO_DATA_DIR"
-    echo "Directory '$MOSQUITTO_DATA_DIR' created."
-fi
-
-# Copy mosquitto configuration
-echo "Copy mosquitto configuration"
-cp ./mosquitto.config ./mosquitto/config
-
-# Check if directory node-red exists
+# Check if directory influxdb exists
 if [ -d "$INFLUXDB_DIR" ]; then
   echo "Directory '$INFLUXDB_DIR' already exists."
 else
-  # Create directory node-red
+  # Create directory influxdb
   mkdir -p "$INFLUXDB_DIR"
   echo "Directory '$INFLUXDB_DIR' created."
 fi
